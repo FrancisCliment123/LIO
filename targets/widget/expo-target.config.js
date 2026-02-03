3375ad7f931b340dad8c96957f00714c410f985f
FrancisCliment123 <franciscojavier.climent@alumni.esade.edu>
2026-02-03 12:09:04 +0100

    Revert to pre-widget state: remove all widget/native folders and restore working config

--- targets/widget/expo-target.config.js
+++
@@ -1,9 +0,0 @@
-export const type = "widget";
-export const icon = 'https://github.com/expo.png';
-export const entitlements = { /* Add entitlements */ };
-
-export default config => ({
-  type,
-  icon,
-  entitlements,
-});
